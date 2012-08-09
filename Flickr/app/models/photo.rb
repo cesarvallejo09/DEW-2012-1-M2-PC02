18b6795829334e921f1294f4d8fbd71b1a3b1620
Cesar Vallejo <cesar_vallejo09@hotmail.com>
2012-08-09 17:00:23 -0500

    prueba commit

--- Flickr/app/models/photo.rb
+++ Flickr/app/models/photo.rb
@@ -1,6 +1,9 @@
 class Photo < ActiveRecord::Base
   attr_accessible :machine_tags, :per_page
   
+  validates :machine_tags, :presence => true
+  validates :per_page, :presence => true
+    
   def read_message
     return flickr.interestingness.getList(:tags => params[:tags], :per_page => params[:per_page])
   end  
