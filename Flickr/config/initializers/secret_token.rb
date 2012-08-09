7a4dff6bafb1c26947ad5d7e1ce611686fee977a
Cesar Vallejo <cesar_vallejo09@hotmail.com>
2012-08-09 11:54:38 -0500

    desarrollo

---
+++ Flickr/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flickr::Application.config.secret_token = '9c9634e082dd7bfb8c4971c16304e91ad07f99f04e823f6b6ae1e7ce632e8e8cc03f9172bc4a059d09b363b8f067902c45e804e485681a4e1b87562023ad1c1b'
